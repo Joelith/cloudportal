8655e02264c190473b308927e85a1dabb2aba78c
Joel Nation <joel.nation@outlook.com>
2016-10-18 15:19:35 +1100

    Added logging to provisioner job handler

--- app/jobs/cloud_provisioner_job.rb
+++ app/jobs/cloud_provisioner_job.rb
@@ -5,9 +5,15 @@ class CloudProvisionerJob < ApplicationJob
   	instances = CloudInstance.find(ids)
   	# Now provision each of the instances
   	instances.each do |instance|
+  		logger.debug "Provisioning instance: #{instance.inspect}"
 			instance.provision
 			instance.wait
   		instance.update(status: 'PROVISIONED')
+  		logger.debug "Instance updated"
   	end
+  end
+
+  rescue_from(StandardError) do |exception|
+  	logger.fatal "Exception in Job: #{exception.inspect}"
   end
 end
